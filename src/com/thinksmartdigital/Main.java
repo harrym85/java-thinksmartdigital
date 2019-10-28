64f0e26203e8f0a6c913a46b30b26633c55a7f4a
thembhani mapengo <harrymapengo@gmail.com>
2019-10-28 10:29:22 +0200

    java_t

--- src/com/thinksmartdigital/Main.java
+++ src/com/thinksmartdigital/Main.java
@@ -3,6 +3,37 @@ package com.thinksmartdigital;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("harry");
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum  Value = " + myMaxIntValue);
+        System.out.println("busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Bused MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest =  2147483647;
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L ;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2147483647_234L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        // byte myNewByteValue = (myMinByteValue / 2 );
+
+
     }
 }
